1151b148f322478d90ee914e3d83a7d83b11a586
vektoreyya <violetgrape47@gmail.com>
2023-03-11 19:41:43 +0200

    Added methods for models2

--- Models/Word.cs
+++ Models/Word.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace words.Models
+﻿namespace words.Models
 {
     internal class Word
     {
@@ -23,7 +17,7 @@ namespace words.Models
 
         public override string ToString()
         {
-            return $"{Title} - {Translation} : {Level}%";
+            return $"{Title} - {Translation}";
         }
     }
 }
