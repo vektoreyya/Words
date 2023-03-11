1009b176af73fbe9058da208ba404488a1109565
vektoreyya <violetgrape47@gmail.com>
2023-03-11 19:59:28 +0200

    Added description property to the language class

--- Models/Language.cs
+++ Models/Language.cs
@@ -11,12 +11,14 @@ namespace words.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public List<Word> Words { get; set; }
+        public String Description { get; set; }
 
-        public Language(Guid id, string title, List<Word> words)
+        public Language(Guid id, string title, List<Word> words, string description)
         {
             Id = id;
             Title = title;
             Words = words;
+            Description = description;
         }
 
         public double getAverageLevel()
